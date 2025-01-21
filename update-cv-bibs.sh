2af8a63018ebd9da981c3266239c4175715607c1
AdebayoBraimah <adebayo.braimah@gmail.com>
2025-01-21 22:20:04 +0000

    Deploying to gh-pages from @ AdebayoBraimah/adebayobraimah.github.io@9b3634c548f797289d8c585224a98cf1fa30575c 🚀

---
+++ update-cv-bibs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# TODO: 
+#   - Move git submodule repo to main level
+
+script_dir=$(realpath $(dirname "${0}"))
+_cwd=$(pwd)
+
+cd ${script_dir}
+
+# Bibliography files
+in_bib_file=${script_dir}/assets/pdf/Braimah-CV/BraimahCV.bib
+out_bib_file=${script_dir}/_bibliography/papers.bib
+
+# CV files
+in_cv_file=${script_dir}/assets/pdf/Braimah-CV/BraimahCV.pdf
+out_cv_file=${script_dir}/assets/pdf/cv_ab.pdf
+
+# Update git submodules
+git pull --recurse-submodules           # Pulls remote repo changes to local repo
+git submodule update --remote --merge   # Any remote changes will now be merged with your local repo
+
+# Git submodules
+cd ${script_dir}/assets/pdf/Braimah-CV
+
+# Update old files
+cp ${in_bib_file} ${out_bib_file}
+cp ${in_cv_file} ${out_cv_file}
