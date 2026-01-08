c2246fc2e9dcea85733e4d81ea0126826067d5f8
AdebayoBraimah <adebayo.braimah@gmail.com>
2026-01-07 17:42:49 -0700

    REPO: Updated with submodule

--- update-cv-bibs.sh
+++ update-cv-bibs.sh
@@ -17,8 +17,9 @@ in_cv_file=${script_dir}/assets/pdf/Braimah-CV/BraimahCV.pdf
 out_cv_file=${script_dir}/assets/pdf/cv_ab.pdf
 
 # Update git submodules
-git pull --recurse-submodules           # Pulls remote repo changes to local repo
-git submodule update --remote --merge   # Any remote changes will now be merged with your local repo
+# git submodule update --init --recursive      # Initializes, if not already done
+git pull --recurse-submodules                # Pulls remote repo changes to local repo
+git submodule update --remote --merge        # Any remote changes will now be merged with your local repo
 
 # Git submodules
 cd ${script_dir}/assets/pdf/Braimah-CV
